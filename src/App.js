4cf15688aea678b38b0ab23eda9d72e6d9062e23
kevhunte <kevinmhunte@gmail.com>
2020-04-18 11:03:38 -0400

    First Commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <p>
+        Monte Carlo Simulator
+      </p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
