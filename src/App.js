e3b939c349b09d6b5cae8658b169a871bc4f145a
kevhunte <kevinmhunte@gmail.com>
2020-04-19 15:07:02 -0400

    Render functionality

--- src/App.js
+++ src/App.js
@@ -4,24 +4,25 @@ import './App.css';
 
 function App() {
   const dims = 15;
+  const start = Math.floor(dims / 2); // middle coord
   let matrix = [];
   for(let i = 0; i < dims; i++){
     let row = [];
     for(let j = 0; j < dims; j++){
-      row.push('x'); // Do Monte Carlo on these vals. Class will update
+      //row.push(Math.floor(Math.random()*2)); // Put all 0s when ready. Do Monte Carlo on these vals
+      row.push(0);
     }
     matrix.push(row);
   }
+  matrix[start][start] = 1; // Start at the middle
 
   return (
     <div className="App">
     <p style={{fontSize: 'calc(20px + 2vmin)', marginTop: '0px', paddingTop: '3rem'}}>
       Monte Carlo Simulator
     </p>
-    <div id="GraphContainer" className="">
-
-    <Grid matrix={matrix}/>
-
+    <div id="GraphContainer">
+      <RenderGrid matrix={matrix}/>
     </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -41,15 +42,15 @@ function App() {
   );
 }
 
-function Grid(props){
+// TODO: Make Monte Carlo func and call RenderGrid in loop to force refresh after update. Remove from App
+
+function RenderGrid(props){
   const matrix = props.matrix;
-  // show nothing for 0, blue spot for 1.
-  //Controlled by 'cell' className
   return (
     matrix.map((row,indexi) => (
         <div key={indexi} className="rowContainer">
           {row.map((col,indexj) => (
-            <div key={indexj} className={col === 1 ? "walked" : ""}>{col}</div>
+            <div key={indexj} className={col === 1 ? "cell walked" : "cell notWalked"}>{col}</div>
           ))}
         </div>
     ))
