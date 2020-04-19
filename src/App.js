7f683ffdbfb582441bf9f5be417db26eee06a573
kevhunte <kevinmhunte@gmail.com>
2020-04-19 14:11:24 -0400

    Grid creation

--- src/App.js
+++ src/App.js
@@ -3,13 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const dims = 15;
+  let matrix = [];
+  for(let i = 0; i < dims; i++){
+    let row = [];
+    for(let j = 0; j < dims; j++){
+      row.push('x'); // Do Monte Carlo on these vals. Class will update
+    }
+    matrix.push(row);
+  }
+
   return (
     <div className="App">
+    <p style={{fontSize: 'calc(20px + 2vmin)', marginTop: '0px', paddingTop: '3rem'}}>
+      Monte Carlo Simulator
+    </p>
+    <div id="GraphContainer" className="">
+
+    <Grid matrix={matrix}/>
+
+    </div>
       <header className="App-header">
-      <p style={{fontSize: 'calc(20px + 2vmin)'}}>
-        Monte Carlo Simulator
-      </p>
-      ---Put Graph Component here---
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -24,6 +38,21 @@ function App() {
         </a>
       </header>
     </div>
+  );
+}
+
+function Grid(props){
+  const matrix = props.matrix;
+  // show nothing for 0, blue spot for 1.
+  //Controlled by 'cell' className
+  return (
+    matrix.map((row,indexi) => (
+        <div key={indexi} className="rowContainer">
+          {row.map((col,indexj) => (
+            <div key={indexj} className={col === 1 ? "walked" : ""}>{col}</div>
+          ))}
+        </div>
+    ))
   );
 }
 
