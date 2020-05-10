2d29eac3b9db69c37dca37c3f316522769117bf5
kevhunte <kevinmhunte@gmail.com>
2020-05-10 15:58:45 -0400

    Work on reset next

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import React, {useState, useCallback, useRef} from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
-//let matrix = [];
-const dims = 21;
+const dims = 41;
+const GRIDDIMENSION = '15px';
 const start = Math.floor(dims / 2); // middle coord
 let currStep = [start,start];
+let steps = 0;
 let interval = 100;
 const BLUETHEME = '#4FD3CB';
 
+const initGrid = () => {
+   const rows = [];
+   for(let i = 0; i < dims; i++){
+    rows.push(Array.from(Array(dims), () => 0))
+  }
+  return rows;
+};
+
 const App = () => {
   //let timer = null;
-   const [matrix, setMatrix] = useState(() => {
-     const rows = [];
-     for(let i = 0; i < dims; i++){
-      rows.push(Array.from(Array(dims), () => 0))
-    }
-    return rows;
-  });
+  const [matrix, setMatrix] = useState(initGrid);
 
   const [isRunning, setRunning] = useState(false);
   matrix[start][start] = 1; // Start at the middle
@@ -29,10 +32,10 @@ const App = () => {
     if(!runningRef.current){
       return;
     }
-    Walk(matrix, setMatrix);
+    Walk(matrix, setMatrix, setRunning);
 
     setTimeout(runSim, interval);
-  },[]);
+  });
 
   return (
     <div className="App">
@@ -43,40 +46,31 @@ const App = () => {
     <div id="GraphContainer">
       <RenderGrid matrix={matrix}/>
     </div>
-    <button
-    style={{
-      padding:'1rem',
-      fontWeight:'bold',
-      color:'white',
-      borderRadius: '25px',
-      backgroundColor:BLUETHEME
-    }}
-    onClick={() => {
-      setRunning(!isRunning);
-      runningRef.current = true;
-      runSim();
-    }}
-    >{isRunning ? 'Stop Simulation' : 'Start Simulation'}</button>
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      padding:'0 0 2rem 0'
+    }}>
+    {steps > 0 ? 'Steps travelled: '+steps : ''} <br/>
+      <button
+        style={{
+          padding:'1rem',
+          fontWeight:'bold',
+          color:'white',
+          borderRadius: '25px',
+          backgroundColor:BLUETHEME
+        }}
+        onClick={() => {
+          setRunning(!isRunning);
+          runningRef.current = true;
+          runSim();
+        }}
+        >{isRunning ? 'Pause Simulation' : 'Start Simulation'}
+      </button>
+    </div>
     </div>
   );
 }
 
-function Walk(matrix, setMatrix){
+function Walk(matrix, setMatrix, setRunning){
   //console.log('walking');
   let possibleSteps = [];
   if(currStep[0] + 1 < dims && !matrix[currStep[0]+1][currStep[1]]){ // can go down
@@ -92,28 +86,33 @@ function Walk(matrix, setMatrix){
     possibleSteps.push([currStep[0],currStep[1]-1]);
   }
 
-  if(!possibleSteps.length){ // nowhere to walk, should stop sim
-    console.log('cleared');
-  }
   let pick = Math.floor(Math.random() * possibleSteps.length);
   //console.log(possibleSteps, pick);
   let stepTaken = possibleSteps.splice(pick, 1)[0]; // randomly pick
-  console.log('step:',stepTaken[0],stepTaken[1]);
 
-  matrix[stepTaken[0]][stepTaken[1]] = 1; // walk
+  if(!stepTaken){ // nowhere to walk, should stop sim
+    console.log('MonteCarlo stopped');
+    setRunning(false);
+  }
+  else{
+    //console.log('step:',stepTaken[0],stepTaken[1]);
 
-  // copies array and updates state
-  let newMatrix = [];
-  matrix.map(rows => {
-    let row =[];
-    rows.map(cols => {
-      row.push(cols);
-    })
-    newMatrix.push(row);
-  });
-  setMatrix(newMatrix);
+    matrix[stepTaken[0]][stepTaken[1]] = 1; // walk
 
-  currStep = stepTaken; // update step
+    // copies array and updates state
+    let newMatrix = [];
+    matrix.map(rows => {
+      let row =[];
+      rows.map(cols => {
+        row.push(cols);
+      })
+      newMatrix.push(row);
+    });
+    setMatrix(newMatrix);
+
+    currStep = stepTaken; // update step
+    steps += 1;
+  }
 }
 
 function RenderGrid(props){
@@ -121,7 +120,7 @@ function RenderGrid(props){
   return (
     <div style={{
       display:'grid',
-      gridTemplateColumns: 'repeat('+dims+', '+25+'px'+')',
+      gridTemplateColumns: 'repeat('+dims+', '+GRIDDIMENSION+')',
       justifyContent: 'center'
     }}>
       {matrix.map((rows,i) =>
@@ -129,8 +128,8 @@ function RenderGrid(props){
           <div
           key={i+'-'+j}
           style={{
-            width:'25px',
-            height:'25px',
+            width:GRIDDIMENSION,
+            height:GRIDDIMENSION,
             backgroundColor: matrix[i][j] ? BLUETHEME : undefined,
             border:'solid 1px '+BLUETHEME
           }}
