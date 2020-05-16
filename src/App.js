5e706c88a8dadd207e859265e08e6bb3d088a77e
kevhunte <kevinmhunte@gmail.com>
2020-05-16 18:39:39 -0400

    Refactor + graph rerender
    --repeated runs?

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useRef} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import {Line} from 'react-chartjs-2';
 import './App.css';
 
@@ -10,12 +10,15 @@ const GRIDDIMENSION = '15px';
 const start = Math.floor(dims / 2); // middle coord
 const BLUETHEME = '#4FD3CB';
 let maxSteps = 0;
+let runs = 0;
+let runCompleted;
 // ------
 
 const initGrid = () => {
   currStep = [start,start];
   steps = 0;
    const rows = [];
+   runCompleted = false;
    for(let i = 0; i < dims; i++){
     rows.push(Array.from(Array(dims), () => 0))
   }
@@ -29,35 +32,100 @@ const App = () => {
   const [isRunning, setRunning] = useState(false);
 
   const [pageController, setpageController] = useState(0);
+
+  const [graphData, setgraphData] = useState({});
+
+  const [prevRuns, setprevRuns] = useState([{'runs':runs,'steps':steps}]);
+
   matrix[start][start] = 1; // Start at the middle
 
   const runningRef = useRef();
   runningRef.current = isRunning;
 
+  const Walk = () => {
+    //console.log('walking');
+    let possibleSteps = [];
+    if(currStep[0] + 1 < dims && !matrix[currStep[0]+1][currStep[1]]){ // can go down
+      possibleSteps.push([currStep[0]+1,currStep[1]]);
+    }
+    if(currStep[0] - 1 >= 0 && !matrix[currStep[0]-1][currStep[1]]){ // can go up
+      possibleSteps.push([currStep[0]-1,currStep[1]]);
+    }
+    if(currStep[1] + 1 < dims && !matrix[currStep[0]][currStep[1]+1]){ // can go right
+      possibleSteps.push([currStep[0],currStep[1]+1]);
+    }
+    if(currStep[1] - 1 >= 0 && !matrix[currStep[0]][currStep[1]-1]){ // can go left
+      possibleSteps.push([currStep[0],currStep[1]-1]);
+    }
+
+    let pick = Math.floor(Math.random() * possibleSteps.length);
+    //console.log(possibleSteps, pick);
+    let stepTaken = possibleSteps.splice(pick, 1)[0]; // randomly pick
+
+    if(!stepTaken){ // nowhere to walk, should stop sim
+      //console.log('MonteCarlo stopped');
+      if(steps > maxSteps) maxSteps = steps;
+      //prevRuns.push({'steps':steps,'runs':runs}); // update state
+      //runs += 1;
+
+      if(!runCompleted){
+        setprevRuns([...prevRuns,{'steps':steps,'runs':runs+=1}]);
+        //console.log('new val:',prevRuns);
+      }
+      runCompleted = true;
+      setRunning(false);
+    }
+    else{
+      //console.log('step:',stepTaken[0],stepTaken[1]);
+
+      matrix[stepTaken[0]][stepTaken[1]] = 1; // walk
+
+      // copies array and updates state
+      let newMatrix = [];
+      matrix.forEach(rows => {
+        let row =[];
+        rows.forEach(cols => {
+          row.push(cols);
+        })
+        newMatrix.push(row);
+      });
+      setMatrix(newMatrix);
+
+      currStep = stepTaken; // update step
+      steps += 1;
+    }
+  }
+
+
   const runSim = () => {
     if(!runningRef.current){
       return;
     }
-    Walk(matrix, setMatrix, setRunning);
+    Walk();
 
     setTimeout(runSim, interval);
   };
 
-  const state = {
-  labels: ['January', 'February', 'March',
-           'April', 'May'],
-  datasets: [
-    {
-      label: 'Rainfall',
-      fill: false,
-      lineTension: 0.5,
-      backgroundColor: 'rgba(75,192,192,1)',
-      borderColor: 'rgba(0,0,0,1)',
-      borderWidth: 2,
-      data: [65, 59, 80, 81, 56]
-    }
-  ]
-}
+  const graph = () => {
+    setgraphData({
+      labels: prevRuns.map((x) => x.runs), //['January', 'February', 'March', 'April', 'May'], // prevRuns.runs
+      datasets: [
+        {
+          label: 'Steps Taken',
+          fill: false,
+          lineTension: 0.5,
+          backgroundColor: BLUETHEME,
+          borderColor: 'rgba(0,0,0,1)',
+          borderWidth: 2,
+          data: prevRuns.map((x) => x.steps) //[65, 59, 80, 81, 56] // prevRuns.steps
+        }
+      ]
+    })
+  };
+
+useEffect(() => {
+  graph();
+},[prevRuns]); // renders when prevRuns is updated
 
   return (
     <div className="App">
@@ -80,68 +148,85 @@ const App = () => {
     }}
     > {pageController === 0 ? 'See all runs =>' : '<= Simulator'} </button>
 
-    <div id="GraphContainer" style={{display: pageController === 0 ? undefined : 'none'}} className="animated fadeIn">
-      <RenderGrid matrix={matrix}/>
-      <div style={{ padding:'0 0 2rem 0'}}>
-        <div style={{padding: '1rem 0', display: steps || maxSteps ? undefined : 'none'}}>
-        {steps > 0 ? 'Steps travelled: '+steps : ''} <br/>
-        {maxSteps ? 'Best run: '+maxSteps : '' } <br/>
+      <div id="GraphContainer" style={{display: pageController === 0 ? undefined : 'none'}} className="animated fadeIn">
+        <RenderGrid matrix={matrix}/>
+        <div style={{ padding:'0 0 2rem 0'}}>
+          <div style={{padding: '1rem 0', display: steps || maxSteps ? undefined : 'none'}}>
+          {steps > 0 ? 'Steps travelled: '+steps : ''} <br/>
+          {maxSteps ? 'Best run: '+maxSteps : '' } <br/>
+          </div>
+          <button
+            style={{
+              padding:'1rem',
+              fontWeight:'bold',
+              color:'white',
+              borderRadius: '25px',
+              backgroundColor:BLUETHEME
+            }}
+            onClick={() => {
+              setRunning(!isRunning);
+              runningRef.current = true;
+              runSim();
+            }}
+            >{isRunning ? 'Pause Simulation' : 'Start Simulation'}
+          </button>
+          <br/>
+          <button
+            style={{
+              padding:'1rem',
+              fontWeight:'bold',
+              color:'white',
+              borderRadius: '25px',
+              backgroundColor:'red',
+              display: !isRunning ? 'inline-block' : 'none'
+            }}
+            onClick={() => {
+              setMatrix(initGrid);
+              //console.log('reset everything here');
+            }}
+            >Reset Simulation
+          </button>
         </div>
-        <button
-          style={{
-            padding:'1rem',
-            fontWeight:'bold',
-            color:'white',
-            borderRadius: '25px',
-            backgroundColor:BLUETHEME
-          }}
-          onClick={() => {
-            setRunning(!isRunning);
-            runningRef.current = true;
-            runSim();
-          }}
-          >{isRunning ? 'Pause Simulation' : 'Start Simulation'}
-        </button>
-        <br/>
-        <button
-          style={{
-            padding:'1rem',
-            fontWeight:'bold',
-            color:'white',
-            borderRadius: '25px',
-            backgroundColor:'red',
-            display: !isRunning ? 'inline-block' : 'none'
-          }}
-          onClick={() => {
-            setMatrix(initGrid);
-            //console.log('reset everything here');
-          }}
-          >Reset Simulation
-        </button>
       </div>
-    </div>
 
-    <div id="ChartContainer" style={{display: pageController !== 0 ? undefined : 'none'}} className="animated fadeIn">
-      <Line
-          data={state}
-          options={{
-            title:{
-              display:true,
-              text:'MonteCarlo Runs Over Time',
-              fontSize:20
-            },
-            legend:{
-              display:true,
-              position:'right'
-            }
-          }}
-        />
-    </div>
+      <div id="ChartContainer" style={{display: pageController !== 0 ? undefined : 'none'}} className="animated fadeIn">
+        <Line
+            data={graphData}
+            options={{
+              title:{
+                display:true,
+                text:'Runs Over Time',
+                fontSize:20,
+                borderColor: 'rgba(0,0,0,1)'
+              },
+              scales:{
+                yAxes:[
+                  {
+                  gridLines:{
+                    display: false
+                  }
+                }
+                ],
+                xAxes:[
+                  {
+                  gridLines:{
+                    display: false
+                  }
+                }
+                ]
+              },
+              legend:{
+                display:true,
+                position:'bottom'
+              }
+            }}
+          />
+      </div>
     </div>
   );
 }
 
-function Walk(matrix, setMatrix, setRunning){
+/*function Walk(matrix, setMatrix, setRunning){
   //console.log('walking');
   let possibleSteps = [];
   if(currStep[0] + 1 < dims && !matrix[currStep[0]+1][currStep[1]]){ // can go down
@@ -164,6 +249,8 @@ function Walk(matrix, setMatrix, setRunning){
   if(!stepTaken){ // nowhere to walk, should stop sim
     //console.log('MonteCarlo stopped');
     if(steps > maxSteps) maxSteps = steps;
+    prevRuns.push({'steps':steps,'runs':runs});
+    runs += 1;
     setRunning(false);
   }
   else{
@@ -185,7 +272,7 @@ function Walk(matrix, setMatrix, setRunning){
     currStep = stepTaken; // update step
     steps += 1;
   }
-}
+}*/
 
 function RenderGrid(props){
   const matrix = props.matrix;
